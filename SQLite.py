89b86aef0bd656bee4d7aa130642e88e265f4119
Ruben van der Steen <ruben@primavista.nl>
2021-11-01 12:27:27 +0100

    updated SQLite.py

--- SQLite.py
+++ SQLite.py
@@ -6,18 +6,24 @@ conn = sqlite3.connect('test.db')
 
 print("Opened database successfully");
 
-conn.execute('''CREATE TABLE IF NOT EXISTS 'users' (
-  'user_id' SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+conn.execute('''DROP TABLE IF EXISTS 'users' ''')
+conn.execute('''CREATE TABLE 'users' (
+  'user_id' INTEGER PRIMARY KEY,
   'role' VARCHAR(50) NULL,
   'user_name' VARCHAR(50) NULL,
   'salt' VARCHAR(255) NULL,
   'password' VARCHAR(255) NULL,
   'RFID' INT NULL,
   'Failed_Login_Attempts' INT NULL,
-  'locked' BOOL NULL,
-  PRIMARY KEY ('user_id'))''')
+  'locked' BOOL NULL
+  )''')
 
+conn.execute('''INSERT INTO 'users' ('role', 'user_name', 'salt', 'password', 'rfid', 'Failed_Login_Attempts', 'locked' )
+	VALUES ('1','rubenvds', 'somesalt','mypassword',003930, 0, 0)''')
 
+cursor = conn.execute("SELECT * from `users`")
+for row in cursor:
+   print(row)
 print("Table created successfully");
-
+conn.commit()
 conn.close()
