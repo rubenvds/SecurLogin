0e2da81c7ea38afa5e182fe6fbcea7b208726951
Daniel Kuiper <daniel.kuiper@live.nl>
2021-11-01 17:05:57 +0100

    scanning frontend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+# Imports
+from flask import Flask, render_template, request, redirect
 import threading
 app = Flask(__name__)
 
@@ -10,14 +11,18 @@ class Message():
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
+    msg = None
     if request.method == "POST":
         # CHECK WITH DATABASE
+        username = request.form.get('username')
+        password = request.form.get('password')
         rfid = input()
-        user = 'admin'
-        if user == 'admin':
-            return redirect('admindashboard')
-        return redirect('userdashboard')
-    msg = None
+        # Log in with right 
+        return redirect('admin')
+        return redirect('user')
+        # If incorrect login ->
+        msg = Message('danger', 'Username/password/RFID incorrect')
+    
     return render_template('login.html', title='Login', msg=msg)
 
 @app.route('/user')
@@ -26,7 +31,8 @@ def userdashboard():
 
 @app.route('/admin')
 def admindashboard():
-    return render_template('admin.html', title='Admin Dashboard')
+    msg = None
+    return render_template('admin.html', title='Admin Dashboard', msg=msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
